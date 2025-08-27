78d4983e25fd15a299aa8461f762b2a98494da08
Inlinera <ilkvakovv@gmail.com>
2025-08-27 19:15:50 +0500

    feat(ui): inputs

--- frontend/src/app/router/App.tsx
+++ frontend/src/app/router/App.tsx
@@ -1,10 +1,18 @@
-import { Header } from "@/widgets/header";
 import { Routes } from "react-router-dom";
+import { Header } from "@/widgets/header";
+import { Input, InputDate, Textarea } from "@/shared/ui/input/";
 
 export const App = () => {
   return (
     <main>
       <Header />
+      <div style={{ marginTop: 100, padding: 10 }}>
+        <InputDate
+          label="Название мероприятия"
+          required
+          placeholder="Введите название мероприятия"
+        />
+      </div>
       <Routes></Routes>
     </main>
   );
