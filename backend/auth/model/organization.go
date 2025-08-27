3bbc3cdd1772f30fa8446374f6a7a6c7ff33a6ee
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2025-08-27 16:26:18 +0300

    feat(auth repository): UpdateUser and CreateUser

--- backend/auth/model/organization.go
+++ backend/auth/model/organization.go
@@ -1,11 +1,11 @@
 package model
 
 type Organization struct {
-	ID            int           `json:"id"`
-	Password      string        `json:"-"`
-	Email         string        `json:"email"`
-	EventsCreated int           `json:"events_created"`
-	HelpersCount  int           `json:"helpers_count"`
-	HoursOfHelp   int           `json:"hours_of_help"`
-	Events        []interface{} `json:"events"`
+	ID            int           `json:"id" db:"id"`
+	Password      string        `json:"-" db:"password"`
+	Email         string        `json:"email" db:"email"`
+	EventsCreated int           `json:"events_created" db:"events_created"`
+	HelpersCount  int           `json:"helpers_count" db:"helpers_count"`
+	HoursOfHelp   int           `json:"hours_of_help" db:"hours_of_help"`
+	Events        []interface{} `json:"events" db:"events"`
 }
--- backend/auth/model/volunteer.go
+++ backend/auth/model/volunteer.go
@@ -1,12 +1,12 @@
 package model
 
 type Volunteer struct {
-	ID                int           `json:"id"`
-	Username          string        `json:"username"`
-	Password          string        `json:"-"`
-	Email             string        `json:"email"`
-	EventsVisited     int           `json:"events_visited"`
-	HoursOfHelp       int           `json:"hours_of_help"`
-	HistoryOfActivity []interface{} `json:"history_of_activity"`
-	CurrentActivity   []interface{} `json:"current_activity"`
+	ID                int           `json:"id" db:"id"`
+	Username          string        `json:"username" db:"username"`
+	Password          string        `json:"-" db:"password"`
+	Email             string        `json:"email" db:"email"`
+	EventsVisited     int           `json:"events_visited" db:"events_visited"`
+	HoursOfHelp       int           `json:"hours_of_help" db:"hours_of_help"`
+	HistoryOfActivity []interface{} `json:"history_of_activity" db:"history_of_activity"`
+	CurrentActivity   []interface{} `json:"current_activity" db:"current_activity"`
 }
