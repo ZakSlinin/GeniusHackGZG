9595ffaad1a886996e7b7a614dbdb4ab2486093b
Inlinera <ilkvakovv@gmail.com>
2025-08-28 23:51:46 +0500

    fix

--- frontend/src/pages/create/ui/createEvent.tsx
+++ frontend/src/pages/create/ui/createEvent.tsx
@@ -6,14 +6,37 @@ import { DEFAULT_VALUES as defaultValues } from "../model/constants";
 import { BasicInfoSection } from "./components/BasicInfo.tsx";
 import { ContactInfoSection } from "./components/ContactInfo.tsx";
 import { VolunteerRolesSection } from "./components/VolunteerRoles.tsx";
+import axios from "axios";
+import { authApi } from "@/shared/store/auth.ts";
 
 export const CreateNewEvent = () => {
   const methods = useForm<Ievent>({
     defaultValues,
   });
 
-  const createEvent = methods.handleSubmit((data) => {
-    console.log("create event data:", data);
+  const createEvent = methods.handleSubmit(async (data) => {
+    data.createdBy = authApi.user?.user;
+
+    data.volunteerNeedCount =
+      data.volunteerGroups?.reduce(
+        (acc, role) => acc + (Number(role.needed) || 0),
+        0
+      ) ?? 0;
+
+    const response = await axios.post(
+      "http://localhost:8080/create-event",
+      data,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    console.log("Event created successfully:", response.data);
+    methods.reset();
+
+    location.href = "/";
   });
 
   return (
