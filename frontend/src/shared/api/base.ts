e8483e167c04a79c8044456a7b51df8d78a977e4
gwizxs <gwizxs@gmail.com>
2025-08-28 06:50:45 +0300

    feat: add axios types and base API configuration

---
+++ frontend/src/shared/api/base.ts
@@ -0,0 +1,18 @@
+import axios, { type AxiosInstance } from "axios";
+
+
+function createInstance(): AxiosInstance {
+    const instance = axios.create({
+        withCredentials: true,
+        baseURL: import.meta.env.VITE_BASE_URL,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    instance.interceptors.request.use((config) => {
+        return config;
+    });
+    return instance;
+}
+
+export const baseInstance = createInstance();
